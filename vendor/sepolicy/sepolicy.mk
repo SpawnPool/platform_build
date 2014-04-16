ccf2f6d923d976211476c58a4e4bfc1cd395ef2c
broodplank <guichardflorent@gmail.com>
2014-04-16 16:29:55 +0200

    Sync SEPolicy with CM11
    Conflicts:
    	vendor/sepolicy/sepolicy.mk
    	vendor/sepolicy/vold.te

--- vendor/sepolicy/sepolicy.mk
+++ vendor/sepolicy/sepolicy.mk
@@ -9,5 +9,9 @@ BOARD_SEPOLICY_DIRS += \
 BOARD_SEPOLICY_UNION += \
     file_contexts \
     fs_use \
+    genfs_contexts \
     seapp_contexts \
+    installd.te \
+    ueventd.te \
+    vold.te \
     mac_permissions.xml
