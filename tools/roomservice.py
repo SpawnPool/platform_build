LineageOS/android_build

make compatible with github v3 api
Change-Id: Iff6f1f9099cdc5d2b49e04000b5fe3d04aa5d7e4

tools/roomservice.py
@@ -21,10 +21,11 @@ repositories = []
 
 page = 1
 while not depsonly:
-    result = json.loads(urllib2.urlopen("http://github.com/api/v2/json/repos/show/CyanogenMod?page=%d" % page).read())
-    if len(result['repositories']) == 0:
+    result = json.loads(urllib2.urlopen("https://api.github.com/users/CyanogenMod/repos?page=%d" % page).read())
+    if len(result) == 0:
         break
-    repositories = repositories + result['repositories']
+    for res in result:
+        repositories.append(res)
     page = page + 1
 
 def exists_in_tree(lm, repository):

SpawnPool/platform_build

Conflicts:
	tools/roomservice.py

tools/roomservice.py
@@ -21,10 +21,11 @@ repositories = []
 
 page = 1
 while not depsonly:
-    result = json.loads(urllib2.urlopen("http://github.com/api/v2/json/repos/show/SpawnPool=%d" % page).read())
-    if len(result['repositories']) == 0:
+    result = json.loads(urllib2.urlopen("https://api.github.com/users/SpawnPool/repos?page=%d" % page).read())
+    if len(result) == 0:
         break
-    repositories = repositories + result['repositories']
+    for res in result:
+        repositories.append(res)
     page = page + 1
 
 def exists_in_tree(lm, repository):