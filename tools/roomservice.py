LineageOS/android_build

roomservice: Bump github request per_page to 100
Change the number of repos per page from the default 30
to 100.

We seem to be hitting the rate limit on the jenkins server.

Change-Id: Ie733feaa0414cbfebb7efcfc1e24d94e1e466d1b

tools/roomservice.py
@@ -39,7 +39,7 @@ repositories = []
 
 page = 1
 while not depsonly:
-    result = json.loads(urllib2.urlopen("https://api.github.com/users/CyanogenMod/repos?page=%d" % page).read())
+    result = json.loads(urllib2.urlopen("https://api.github.com/users/CyanogenMod/repos?per_page=100&page=%d" % page).read())
     if len(result) == 0:
         break
     for res in result:

SpawnPool/platform_build

Conflicts:
	tools/roomservice.py

tools/roomservice.py
@@ -25,7 +25,7 @@ repositories = []
 
 page = 1
 while not depsonly:
-    result = json.loads(urllib2.urlopen("https://api.github.com/users/SpawnPool/repos?page=%d" % page).read())
+    result = json.loads(urllib2.urlopen("https://api.github.com/users/SpawnPool/repos?per_page=100&page=%d" % page).read())
     if len(result) == 0:
         break
     for res in result: